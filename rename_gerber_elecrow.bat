c3e0043953cbcb712465f2d52ef52b1088025abd
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2023-04-29 01:55:45 +0900

    KiCad の出力するファイル名が …-F_SilkS.ghr から …-F_Silkscreen.gbr に変わったんか

--- rename_gerber_elecrow.bat
+++ rename_gerber_elecrow.bat
@@ -8,8 +8,8 @@ copy "%PROJECT%"-F_Cu.gbr "%OUTPUT%"\"%PROJECT%".GTL
 copy "%PROJECT%"-B_Cu.gbr "%OUTPUT%"\"%PROJECT%".GBL
 copy "%PROJECT%"-F_Mask.gbr "%OUTPUT%"\"%PROJECT%".GTS
 copy "%PROJECT%"-B_Mask.gbr "%OUTPUT%"\"%PROJECT%".GBS
-copy "%PROJECT%"-F_SilkS.gbr "%OUTPUT%"\"%PROJECT%".GTO
-copy "%PROJECT%"-B_SilkS.gbr "%OUTPUT%"\"%PROJECT%".GBO
+copy "%PROJECT%"-F_Silkscreen.gbr "%OUTPUT%"\"%PROJECT%".GTO
+copy "%PROJECT%"-B_Silkscreen.gbr "%OUTPUT%"\"%PROJECT%".GBO
 copy "%PROJECT%"-NPTH.drl "%OUTPUT%"\"%PROJECT%"-NPTH.TXT
 copy "%PROJECT%"-PTH.drl "%OUTPUT%"\"%PROJECT%"-PTH.TXT
 copy "%PROJECT%"-Edge_Cuts.gbr "%OUTPUT%"\"%PROJECT%".GML
